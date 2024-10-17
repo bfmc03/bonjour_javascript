aecac2541cd0b3ce8b9d2724aaf778af651a85de
flo <boireaufloriane@hotmail.fr>
2024-10-17 15:52:09 +0200

    ajout script3.js

---
+++ script3.js
@@ -0,0 +1,10 @@
+function sayHello(naim) {  // je déclare ma fonction sayHello en lui ajoutant un argument main
+    let message = "Bonjour "+ naim // Ke concataine mon argument a ma varible 
+    console.log(message)
+    }
+    
+    sayHello()  // j'apelle ma fonction 
+    
+    let firstname = "beyonce !" // je déclare une autre variable qui devient un paramètre
+    
+    sayHello(firstname)  // J'apelle ma fonction avec son paramètre
